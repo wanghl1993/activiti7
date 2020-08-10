f9b764d4124825b5de8c00c2801c4d557f680963
wanghl <study_yz@163.com>
2020-08-10 15:53:50 +0800

    basic module

--- src/main/java/com/itheima/activiti/ActivitiDeployment.java
+++ src/main/java/com/itheima/activiti/ActivitiDeployment.java
@@ -5,6 +5,9 @@ import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
 
+import java.io.InputStream;
+import java.util.zip.ZipInputStream;
+
 /**
  *  流程定义的部署
  *  activiti表有哪些?
@@ -22,10 +25,17 @@ public class ActivitiDeployment {
         // 2.得到RepositoryService实例
         RepositoryService repositoryService = processEngine.getRepositoryService();
 
+
+        // 3.转化出ZipInputStream流对象
+        InputStream is = ActivitiDeployment.class.getClassLoader()
+                .getResourceAsStream("diagram/holidayBPMN.zip");
+
+        // 将 InputStream流 转化为 ZipInputStream
+        ZipInputStream zipInputStream = new ZipInputStream(is);
+
         // 3.进行部署
         Deployment deployment = repositoryService.createDeployment()
-                .addClasspathResource("diagram/holiday.bpmn") //添加bpmn资源
-                .addClasspathResource("diagram/holiday.png")
+                .addZipInputStream(zipInputStream)
                 .name("请假申请单流程")
                 .deploy();
 
@@ -34,5 +44,25 @@ public class ActivitiDeployment {
         System.out.println(deployment.getId());
 
     }
+
+    //流程定义部署
+//    public static void main(String[] args) {
+//        // 1.创建ProcessEngine对象
+//        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//        // 2.得到RepositoryService实例
+//        RepositoryService repositoryService = processEngine.getRepositoryService();
+//
+//        // 3.进行部署
+//        Deployment deployment = repositoryService.createDeployment()
+//                .addClasspathResource("diagram/holiday.bpmn") //添加bpmn资源
+//                .addClasspathResource("diagram/holiday.png")
+//                .name("请假申请单流程")
+//                .deploy();
+//
+//        // 4.输出部署的一些信息
+//        System.out.println(deployment.getName());
+//        System.out.println(deployment.getId());
+//
+//    }
 
 }
