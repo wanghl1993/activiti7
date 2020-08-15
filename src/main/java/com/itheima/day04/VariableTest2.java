a6453e3fa4b05f245d805511641b6c7f30712ab1
wanghl1993 <whl1994)!!@>
2020-08-16 00:28:04 +0800

    72 set variables

--- src/main/java/com/itheima/day04/VariableTest2.java
+++ src/main/java/com/itheima/day04/VariableTest2.java
@@ -11,7 +11,7 @@ import java.util.Map;
 /**
  *  流程变量的测试
  */
-public class VariableTest {
+public class VariableTest2 {
 
     // 完成任务 zhangsan --- lisi --- 判断流程变量的请假天数,1天 ---- 分支: 人事经理存档
     public static void main(String[] args) {
@@ -24,11 +24,18 @@ public class VariableTest {
         // 3.查询当前用户是否有任务
         String key = "myProcess_1";
         Task task = taskService.createTaskQuery().processDefinitionKey(key)
-                .taskAssignee("zhaoliu").singleResult();
+                .taskAssignee("zhangsan").singleResult();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        //初始化一些参数
+        Holiday holiday = new Holiday();
+        holiday.setNum(5F);
+        map.put("holiday",holiday);
 
         // 4.判断task != null,说明当前用户有任务
         if(task != null) {
-            taskService.complete(task.getId());
+            taskService.complete(task.getId(),map); //完成任务时,设置流程变量的值
             System.out.println("任务执行完毕");
         }
 
@@ -39,7 +46,7 @@ public class VariableTest {
     //启动流程实例,同时还要设置流程变量的值
     // act_ge_bytearray
     // act_ru_variable
-    public static void main1(String[] args) {
+    public static void main2(String[] args) {
         // 1.得到ProcessEngine
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 
@@ -48,14 +55,14 @@ public class VariableTest {
 
         // 3.流程定义的key问题 myProcess_1
         String key = "myProcess_1";
-        Map<String, Object> map = new HashMap<String, Object>();
+//        Map<String, Object> map = new HashMap<String, Object>();
+//
+//        Holiday holiday = new Holiday();
+//        holiday.setNum(5F);
+//        map.put("holiday",holiday);
 
-        Holiday holiday = new Holiday();
-        holiday.setNum(5F);
-        map.put("holiday",holiday);
-
-        // 4.启动流程实例,并且设置流程变量的值
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(key, map);
+        // 4.启动流程实例
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(key);
 
         // 5.输出实例信息
         System.out.println(processEngine.getName());
@@ -64,22 +71,22 @@ public class VariableTest {
     }
 
     //新的请假流程定义部署
-//    public static void main(String[] args) {
-//        // 1.得到ProcessEngine
-//        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-//
-//        // 2.得到RepositoryService对象
-//        RepositoryService repositoryService = processEngine.getRepositoryService();
-//
-//        // 3.部署
-//        Deployment deploy = repositoryService.createDeployment()
-//                .addClasspathResource("diagram/holiday4.bpmn")
-//                .addClasspathResource("diagram/holiday4.png")
-//                .name("请假流程-流程变量")
-//                .deploy();
-//
-//        System.out.println(deploy.getId());
-//        System.out.println(deploy.getName());
-//    }
+    public static void main1(String[] args) {
+        // 1.得到ProcessEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        // 2.得到RepositoryService对象
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+
+        // 3.部署
+        Deployment deploy = repositoryService.createDeployment()
+                .addClasspathResource("diagram/holiday4.bpmn")
+                .addClasspathResource("diagram/holiday4.png")
+                .name("请假流程-流程变量")
+                .deploy();
+
+        System.out.println(deploy.getId());
+        System.out.println(deploy.getName());
+    }
 
 }
